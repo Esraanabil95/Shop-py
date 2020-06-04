be5d5328a62ef0311d7e34d93fc280c4aa775bc9
Esraanabil95 <esraanabil665@gmail.com>
2020-06-04 15:28:40 +0200

    My first Commit

---
+++ products/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+    # return HttpResponse("Hello World")
+
+
+def new(request):
+    return HttpResponse("new products")
